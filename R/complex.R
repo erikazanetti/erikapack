6d7ce6a6bcc41d7b12ea7839078f1d2d427ff59a
erikazanetti <erikazanetti.25@gmail.com>
2024-12-18 16:39:52 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -2,14 +2,6 @@ complex <- function(x){
 if(x>0){
   print("This is a positive number")
   }
-  else print("This is a negative number")
-  }
-
-
-complex <- function(x){
-if(x>0){
-  print("This is a positive number")
-  }
   else if(x<0){
     print("This is a negative number")
   }
