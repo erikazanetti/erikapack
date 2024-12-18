d60c4e3052e00bb973b2d6a6c78ac9c15a17b59d
erikazanetti <erikazanetti.25@gmail.com>
2024-12-18 15:34:37 +0100

    Update complex.R

--- R/complex.R
+++ R/complex.R
@@ -5,13 +5,6 @@ if(x>0){
   else print("This is a negative number")
   }
 
-# what the function is saying: if the number the is more than 0, state that the number is a positive number, if the number is less than 0, state that the number is a negative number 
-# the function to state something is print()
-# let's try if it works
-complex(65)
-complex(-65)
-
-# what to do with 0?
 
 complex <- function(x){
 if(x>0){
